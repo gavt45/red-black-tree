820abdd7a93c753dc3e3d3f62b647fc8dd2cfea2
Gumirov Andrey <andreyt45@gmail.com>
2020-04-26 13:13:29 +0700

    Initial commit.

---
+++ cmake-build-debug/CMakeFiles/red_black_tree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/red_black_tree.dir/main.cpp.o"
+  "red_black_tree"
+  "red_black_tree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/red_black_tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/red_black_tree.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for red_black_tree.
+# This may be replaced when dependencies are built.
